00f1efc485992d4dd24caa6bd6fe9100ad1abb59
DanielCC <cardozacalderondaniel400@gmail.com>
2026-03-05 21:55:22 -0600

    documentación

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 
+# Función para recibir los mensajes de otros clientes
 def recibir_msg():
     while True:
         try:
@@ -11,15 +12,19 @@ def recibir_msg():
         except:
             break
 
+# Definición de Socket
 cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 cliente.connect(("127.0.0.1", 5000))
 
+# Definición de Hilo
 hilo = threading.Thread(target=recibir_msg)
 hilo.start()
 
+# Loop para enviar mensajes
 while True:
     mensaje = input("Escribe un mensaje: ")
 
+    # No se permiten mensajes vacíos
     if mensaje.strip() == "":
         print("ERROR: No puedes ingresar una entrada vacía")
         continue
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 
+# Función para reenviar un mensaje recibido de parte de un cliente al resto de clientes
 def broadcast(mensaje, emisor=None):
     for cliente in clientes:
         if cliente != emisor:
@@ -9,13 +10,16 @@ def broadcast(mensaje, emisor=None):
             except:
                 clientes.remove(cliente)
 
+# Recibir clientes nuevos
 def manejar_cliente(cliente, direccion):
     print(f"{direccion} se ha conectado")
 
+    # Enviar mensaje y hacer un salto de línea
     with lock:
         for msg in mensajes:
             cliente.send((msg + "\n").encode())
 
+    # Loop para recibir mensajes y llamar a la función de broadcast
     while True:
         try:
             mensaje = cliente.recv(1024).decode()
@@ -35,20 +39,24 @@ def manejar_cliente(cliente, direccion):
         except:
             break
 
+    # Manejar desconexión de cliente
     print(f"{direccion} se ha desconectado")
     clientes.remove(cliente)
     cliente.close()
 
+# Creación de Listas y Lock
 clientes = []
 mensajes = []
 lock = threading.Lock()
 
+# Creación de Socket y empezar la escucha de clientes
 servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 servidor.bind(("127.0.0.1", 5000))
 servidor.listen()
 
 print("Servidor esperando conexión...")
 
+# Loop para aceptar clientes y crear hilos para ellos
 while True:
     cliente, direccion = servidor.accept()
     clientes.append(cliente)
